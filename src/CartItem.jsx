95eee33ee97ec06b0cbdf1ff5ebefe465122099a
Magd13 <joelmagdielriveraarce@gmail.com>
2025-03-29 21:45:33 -0500

    Task Completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0
+    cart.forEach(item => {
+      const totalU = parseFloat((item.cost).replace('$','')) * item.quantity
+      total += totalU
+    });
+    return total
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   const handleContinueShopping = (e) => {
-   
-  };
-
-
+    onContinueShopping(e)
+  }
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity:item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1 ){
+      dispatch(updateQuantity({name: item.name, quantity:item.quantity - 1}))
+    }else {
+      dispatch(removeItem(item.name))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const totalU = (item.cost).replace('$','') * item.quantity
+    return totalU
   };
 
   return (
